30aed1fe4bdf3f991f3f424b1a4e374da10c3213
BeauConstrictor <scabby-gone-boring@duck.com>
2025-11-09 19:57:18 +0000

    mockup of a text editor in python to be ported

---
+++ edit.py
@@ -0,0 +1,144 @@
+# this code looks terrible to a python programmer, but is designed to be easy to
+# port to 6502 assembly, so it inherits some of the quirks of that processor.
+
+import sys
+
+before = ""
+after  = ""
+
+scroll = 5
+
+m_cmd    = 0
+m_insert = 1
+mode     = 0
+
+if sys.platform.startswith("win"):
+    import msvcrt
+
+    def getch() -> str:
+        if msvcrt.kbhit():
+            c = msvcrt.getwch()
+            if c == '\r': c = '\n'
+            return c
+        return chr(0)
+
+else:
+    import select
+    import tty
+    import termios
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setcbreak(fd)
+
+    def getch() -> str:
+        dr, dw, de = select.select([sys.stdin], [], [], 0)
+        if dr:
+            return sys.stdin.read(1)
+        return chr(0)
+
+    import atexit
+    
+    atexit.register(lambda: termios.tcsetattr(fd, termios.TCSADRAIN, old_settings))
+
+def draw() -> None:
+    print("\033[H\033[J\033[3J")
+    
+    if mode == m_cmd:
+        print("--- command mode ---\n")
+    elif mode == m_insert:
+        print("--- insert mode ---\n")
+    
+    scroll_count = scroll
+    for ch in before:
+        if ch == "\n" and scroll_count != 0:
+            scroll_count -= 1
+            continue
+        if scroll_count != 0:
+            continue
+        print(ch, end="")
+    print("\033[7m", end="")
+    if after[0] == "\n":
+        print(" ", end="")
+        print("\n", end="")
+    else:
+        print(after[0], end="")
+    print("\033[0m", end="")
+    for ch in after[1:]:
+        if ch == "\n" and scroll_count != 0:
+            scroll_count -= 1
+        if scroll_count != 0:
+            continue
+        print(ch, end="")
+
+def key_cmd() -> None:
+    global before
+    global after
+    global scroll
+    global mode
+    
+    key = chr(0x00)
+    while key == chr(0x00):
+        key = getch()
+    
+    if key == "J":
+        scroll += 1
+        return
+    if key == "K":
+        scroll -= 1
+        return
+    
+    if key == "h":
+        after = before[-1] + after
+        before = before[:-1]
+        return
+    if key == "l":
+        before = before + after[0]
+        after = after[1:]
+        return
+    
+    if key == "q":
+        print("\033[?25h")
+        exit(0)
+    
+    if key == "i":
+        mode = m_insert
+        return
+
+def key_insert() -> None:
+    global before
+    global after
+    global mode
+    
+    key = chr(0x00)
+    while key == chr(0x00):
+        key = getch()
+        
+    if key == "\033":
+        mode = m_cmd
+        return
+    
+    if key == "\b" or key == "\x7f":
+        before = before[:-1]
+        return
+        
+    before += key
+
+def handle_key() -> None:
+    if mode == m_cmd:
+        key_cmd()
+        return
+    key_insert()
+
+def main() -> None:
+    print("\033[?25l")
+    
+    while True:
+        draw()
+        handle_key()
+
+if __name__ == "__main__":
+    before = "\n".join(str(i) for i in range(10))
+    after  = "\n" + "\n".join(str(i+len(before)//2+1) for i in range(10))
+    
+    main()
