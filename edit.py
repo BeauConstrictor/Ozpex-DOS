d66167b8b8058e2d55d1012a98be79109865da32
BeauConstrictor <scabby-gone-boring@duck.com>
2025-11-10 19:19:19 +0000

    slightly refactored the editor's python mockup to make it easier to implement

--- edit.py
+++ edit.py
@@ -12,7 +12,11 @@ m_cmd    = 0
 m_insert = 1
 mode     = 0
 
-ROWS = 30
+newlines = 0 # can be undefined in the asm
+scroll_count = 0 # can be undefined in the asm
+ch = ""  # can be undefined in the asm
+
+ROWS = 25
 
 if sys.platform.startswith("win"):
     import msvcrt
@@ -43,37 +47,31 @@ else:
     
     atexit.register(lambda: termios.tcsetattr(fd, termios.TCSADRAIN, old_settings))
 
-def draw_footer() -> None:
-    print("\033[7m")
-    if mode == m_cmd:
-        print("\n                               --- normal mode ---                              ")
-    elif mode == m_insert:
-        print("\n                               --- insert mode ---                              ")
-    print("\033[0m", end="")    
-
-def draw() -> None:
-    print("\033[H\033[J\033[3J")
-    
+def draw_header() -> None:
     print("\033[7m", end="")
-    print("                           **** OZDOS EDIT V0.0.0 ****                          \n")
+    print("                           **** OZDOS EDIT V0.0.0 ****                          ")
+    
+    if mode == m_cmd:
+        print("                               --- normal mode ---                              \n")
+    elif mode == m_insert:
+        print("                               --- insert mode ---                              \n")
     print("\033[0m", end="")
     
-    scroll_count = scroll
-    newlines = 0
+
+def print_char() -> None:
+    global scroll_count
+    global newlines
     
-    for ch in before:
-        if ch == "\n" and scroll_count != 0:
-            scroll_count -= 1
-            continue
-        if scroll_count != 0:
-            continue
-        if ch == "\n":
-            newlines += 1
-            if newlines > ROWS:
-                draw_footer()
-                return
-        print(ch, end="")
-        
+    if ch == "\n" and scroll_count != 0:
+        scroll_count -= 1
+        return
+    if scroll_count != 0:
+        return
+    if ch == "\n":
+        newlines += 1
+    print(ch, end="")
+
+def print_cursor() -> None:
     print("\033[7m", end="")
     if after[0] == "\n":
         print(" ", end="")
@@ -81,20 +79,32 @@ def draw() -> None:
     else:
         print(after[0], end="")
     print("\033[0m", end="")
+
+def draw() -> None:
+    global ch
+    global scroll_count
+    global newlines
     
-    for ch in after[1:]:
-        if ch == "\n" and scroll_count != 0:
-            scroll_count -= 1
-        if scroll_count != 0:
-            continue
-        if ch == "\n":
-            newlines += 1
-            if newlines > ROWS:
-                draw_footer()
-                return
-        print(ch, end="")
+    print("\033[H\033[J\033[3J")
+    
+    draw_header()
+    
+    scroll_count = scroll
+    newlines = 0
+    
+    for c in before:
+        ch = c
+        if ch == "\n" and newlines > ROWS:
+            break
+        print_char()
         
-    draw_footer()
+    print_cursor()
+    
+    for c in after[1:]:
+        ch = c
+        if ch == "\n" and newlines > ROWS:
+            break
+        print_char()
 
 def key_cmd() -> None:
     global before
@@ -163,8 +173,8 @@ def main() -> None:
         handle_key()
 
 if __name__ == "__main__":
-    with open("disks/devkit/monitor.asm", "r") as f:
-        before = f.read()
-        scroll = len(before.split("\n")) - ROWS
+    # with open("disks/devkit/monitor.asm", "r") as f:
+    #     before = f.read()
+    #     scroll = len(before.split("\n")) - ROWS
         
     main()
