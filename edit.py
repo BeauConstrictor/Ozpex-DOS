a8dc03ae963881948546a2a7a0f508389881e573
BeauConstrictor <scabby-gone-boring@duck.com>
2025-11-12 22:38:22 +0000

    completly rewrote the editor (python version) (not finished yet)

--- edit.py
+++ edit.py
@@ -3,178 +3,180 @@
 
 import sys
 
-before = ""
-after  = "\n"
+buffer = ["_"] * 8192
+buffer_end = len(buffer) # first byte after buffer
+gap_start = 0 # index of first byte in the gap
+after_gap = 8192 # index of first char after the gap
 
-scroll = 0
+mode = 0
 
-m_cmd    = 0
-m_insert = 1
-mode     = 0
+lines = 0 # uninitialsed # how many lines have been printed by draw
 
-newlines = 0 # can be undefined in the asm
-scroll_count = 0 # can be undefined in the asm
-ch = ""  # can be undefined in the asm
+INSERT = 1
+NORMAL = 0
 
 ROWS = 25
 
 if sys.platform.startswith("win"):
     import msvcrt
-
     def getch() -> str:
         if msvcrt.kbhit():
             c = msvcrt.getwch()
             if c == '\r': c = '\n'
             return c
         return chr(0)
-
 else:
     import select
     import tty
     import termios
-
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     tty.setcbreak(fd)
-
     def getch() -> str:
         dr, dw, de = select.select([sys.stdin], [], [], 0)
         if dr:
             return sys.stdin.read(1)
         return chr(0)
-
     import atexit
-    
     atexit.register(lambda: termios.tcsetattr(fd, termios.TCSADRAIN, old_settings))
 
-def draw_header() -> None:
-    print("\033[7m", end="")
-    print("                           **** OZDOS EDIT V0.0.0 ****                          ")
-    
-    if mode == m_cmd:
-        print("                               --- normal mode ---                              \n")
-    elif mode == m_insert:
-        print("                               --- insert mode ---                              \n")
-    print("\033[0m", end="")
-    
-
-def print_char() -> None:
-    global scroll_count
-    global newlines
-    
-    if ch == "\n" and scroll_count != 0:
-        scroll_count -= 1
-        return
-    if scroll_count != 0:
-        return
-    if ch == "\n":
-        newlines += 1
-    print(ch, end="")
-
-def print_cursor() -> None:
-    print("\033[7m", end="")
-    if after[0] == "\n":
-        print(" ", end="")
-        print("\n", end="")
+def write(ch: str) -> None:
+    if ch == chr(0x11):
+        print("\033[2J\033[H", end="", flush=True, file=sys.stderr)
     else:
-        print(after[0], end="")
-    print("\033[0m", end="")
+        print(ch, end="", flush=True, file=sys.stderr)
+
+def draw_cursor() -> None:    
+        if after_gap == buffer_end:
+            write("█")
+            return
+        if buffer[after_gap] == "\n":
+            write("█")
+            write("\n")
+            return
+        
+        print("\033[7m", end="", file=sys.stderr)
+        write(buffer[after_gap])
+        print("\033[0m", end="", file=sys.stderr)
 
 def draw() -> None:
-    global ch
-    global scroll_count
-    global newlines
+    global lines
     
-    print("\033[H\033[J\033[3J")
+    lines = 0
     
-    draw_header()
+    write(chr(0x11))
     
-    scroll_count = scroll
-    newlines = 0
+    if mode == NORMAL:
+        print("--- normal ---\n", file=sys.stderr)
+    if mode == INSERT:
+        print("--- insert ---\n", file=sys.stderr)
     
-    for c in before:
-        ch = c
-        if ch == "\n" and newlines > ROWS:
-            break
-        print_char()
+    x = 0
+    while True:
+        if x == gap_start: break
+        if buffer[x] == "\n":
+            lines += 1
+            if lines == ROWS: return
+        write(buffer[x])
+        x += 1
+    
+    draw_cursor()
         
-    print_cursor()
-    
-    for c in after[1:]:
-        ch = c
-        if ch == "\n" and newlines > ROWS:
-            break
-        print_char()
+    x = after_gap
+    x += 1
+    while True:
+        if x >= buffer_end: break
+        if buffer[x] == "\n":
+            lines += 1
+            if lines == ROWS: return
+        write(buffer[x])
+        x += 1
 
-def key_cmd() -> None:
-    global before
-    global after
-    global scroll
+def keypress_normal() -> None:
+    global gap_start
+    global after_gap
     global mode
     
-    key = chr(0x00)
-    while key == chr(0x00):
-        key = getch()
+    ch = chr(0)
+    while ch == chr(0):
+        ch = getch()
     
-    if key == "J":
-        scroll += 1
+    if ch == "l":
+        if after_gap == buffer_end: return
+        char = buffer[after_gap]
+        after_gap += 1
+        buffer[gap_start] = char
+        gap_start += 1
         return
-    if key == "K":
-        scroll -= 1
+    if ch == "h":
+        if gap_start == 0: return
+        gap_start -= 1
+        char = buffer[gap_start]
+        after_gap -= 1
+        buffer[after_gap] = char
         return
-    
-    if key == "h":
-        after = before[-1] + after
-        before = before[:-1]
-        return
-    if key == "l":
-        before = before + after[0]
-        after = after[1:]
-        return
-    
-    if key == "q":
-        print("\033[?25h")
+    if ch == "q":
+        export()
+        print("\033[?25h", end="", flush=True, file=sys.stderr)
         exit(0)
-    
-    if key == "i":
-        mode = m_insert
         return
-
-def key_insert() -> None:
-    global before
-    global after
+    if ch == "i":
+        mode = INSERT
+        return
+        
+def keypress_insert() -> None:
+    global gap_start
     global mode
     
-    key = chr(0x00)
-    while key == chr(0x00):
-        key = getch()
+    ch = chr(0)
+    while ch == chr(0):
+        ch = getch()
         
-    if key == "\033":
-        mode = m_cmd
+    if ch == "\033":
+        mode = NORMAL
         return
     
-    if key == "\b" or key == "\x7f":
-        before = before[:-1]
+    if ch == chr(0x08):
+        keypress_insert_backspace()
         return
-        
-    before += key
+    if ch == chr(0x7f):
+        keypress_insert_backspace()
+        return
+    
+    buffer[gap_start] = ch
+    gap_start += 1
 
-def handle_key() -> None:
-    if mode == m_cmd:
-        key_cmd()
+def keypress_insert_backspace() -> None:
+    global gap_start
+    if gap_start == 0: return
+    gap_start -= 1
+
+def keypress() -> None:
+    if mode == NORMAL:
+        keypress_normal()
         return
-    key_insert()
+    keypress_insert()
+
+def export() -> None:
+    x = 0
+    while True:
+        if x == gap_start: break
+        sys.stdout.write(buffer[x])
+        x += 1
+    
+    x = after_gap
+    x += 1
+    while True:
+        if x >= buffer_end: break
+        sys.stdout.write(buffer[x])
+        x += 1
 
 def main() -> None:
-    print("\033[?25l")
+    print("\033[?25l", end="", file=sys.stderr)
     
     while True:
         draw()
-        handle_key()
+        keypress()
 
 if __name__ == "__main__":
-    # with open("disks/devkit/monitor.asm", "r") as f:
-    #     before = f.read()
-    #     scroll = len(before.split("\n")) - ROWS
-        
     main()
